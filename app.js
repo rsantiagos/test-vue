6f2a553249e3277d9600766b946e1d930077b24a
Romualdo Santiago Santiago <rsantiago@inndot.com>
2019-04-09 12:55:13 -0500

    Iniciando

---
+++ app.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: "#main",
+    data:{
+        titulo: "test vue",
+        tareas: []
+    }
+})
---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use(function(req, res, next) {  
+  res.status(404).json({error: 'No se encontró el servicio, por favor verifique su ruta.'});    
+});
+
+app.set('port', process.env.PORT || 3000);   
+
+app.listen(app.get('port'), function () {
+  console.log('Express started on server: ' + app.get('port'));
+});
